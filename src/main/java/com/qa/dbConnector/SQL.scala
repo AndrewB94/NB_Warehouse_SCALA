e244df999ebae6865dd21a977e6ce4a26dafd3ee
AndrewB94 <andrewbutcher94@gmail.com>
2015-11-03 17:22:42 +0000

    scalafx frame

---
+++ src/main/java/com/qa/dbConnector/SQL.scala
@@ -0,0 +1,8 @@
+package com.qa.dbConnector
+
+/**
+ * @author abutcher
+ */
+class SQL {
+  
+}
--- src/main/java/com/qa/gui/Main.scala
+++ src/main/java/com/qa/gui/Main.scala
@@ -1,8 +1,15 @@
 package com.qa.gui
 
-/**
- * @author abutcher
- */
-object Main {
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.stage.Stage
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+object Main extends JFXApp {
+ def start(primaryStage:Stage) {
+   
+ }
+
   
 }
