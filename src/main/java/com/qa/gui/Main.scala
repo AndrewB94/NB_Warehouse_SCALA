28fdd1a561065e551db2a6cb5870e1b9761a2316
AndrewB94 <andrewbutcher94@gmail.com>
2015-11-04 10:14:45 +0000

    fixed scalafx

--- src/main/java/com/qa/gui/Main.scala
+++ src/main/java/com/qa/gui/Main.scala
@@ -1,15 +1,25 @@
 package com.qa.gui
 
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.stage.Stage
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 
 object Main extends JFXApp {
- def start(primaryStage:Stage) {
-   
- }
-
-  
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LightGreen
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when (hover) choose Color.Green otherwise Color.Red
+      }
+    }
+  }
 }
