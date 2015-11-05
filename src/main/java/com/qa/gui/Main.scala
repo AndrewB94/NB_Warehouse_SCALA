415023ed0217ef675c78491c17bc5220cdb25ccf
AndrewB94 <andrewbutcher94@gmail.com>
2015-11-05 16:54:46 +0000

    purchase order list scene

--- src/main/java/com/qa/gui/Main.scala
+++ src/main/java/com/qa/gui/Main.scala
@@ -1,41 +1,17 @@
 package com.qa.gui
 
-import com.qa.loader.PurchaseOrderLoad
+import com.qa.gui.PurchaseOrdersScene
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.collections.ObservableBuffer
+
 import scalafx.scene.Scene
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.{TableCell, TableColumn, TableView}
-import scalafx.scene.paint.Color
-import scalafx.scene.shape.Circle
-import com.qa.Entities.PurchaseOrder
+
 
 
 object Main extends JFXApp {
-  val pol: PurchaseOrderLoad = new PurchaseOrderLoad
-    val orders = pol.getAllPurchaseOrders
-
+    var poScene: PurchaseOrdersScene = new PurchaseOrdersScene
     stage = new PrimaryStage {
-      title = "TableView with custom color cell"
-      scene = new Scene {
-        content = new TableView[PurchaseOrder](orders) {
-          columns ++= List(
-            new TableColumn[PurchaseOrder, String] {
-              text = "ID"
-              cellValueFactory = { _.value.id }
-
-            },
-            new TableColumn[PurchaseOrder, String]() {
-              text = "Status"
-              cellValueFactory = { _.value.status }
-            },
-            new TableColumn[PurchaseOrder, String] {
-              text = "Supplier"
-              cellValueFactory = { _.value.supplier }
-            })
-        }
-      }
+      title = "NB Gardens - Warehouse Order Tracking System"
+      scene = poScene.getScene
     }
-
 }
--- src/main/java/com/qa/gui/PurchaseOrdersScene.scala
+++ src/main/java/com/qa/gui/PurchaseOrdersScene.scala
@@ -1,8 +1,42 @@
 package com.qa.gui
 
+import com.qa.loader.PurchaseOrderLoad
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.Scene
+import scalafx.scene.control.TableColumn._
+import scalafx.scene.control.{ TableCell, TableColumn, TableView }
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
+import com.qa.Entities.PurchaseOrder
+
 /**
  * @author abutcher
  */
 class PurchaseOrdersScene {
-  
+  val pol: PurchaseOrderLoad = new PurchaseOrderLoad
+
+  def getScene: Scene  ={   
+    val orders = pol.getAllPurchaseOrders  
+    var scene = new Scene {
+      content = new TableView[PurchaseOrder](orders) {
+        columns ++= List(
+          new TableColumn[PurchaseOrder, String] {
+            text = "ID"
+            cellValueFactory = { _.value.id }
+            prefWidth = 100
+          },
+          new TableColumn[PurchaseOrder, String]() {
+            text = "Status"
+            cellValueFactory = { _.value.status }
+            prefWidth = 100
+          },
+          new TableColumn[PurchaseOrder, String] {
+            text = "Supplier"
+            cellValueFactory = { _.value.supplier }
+            prefWidth = 100
+          })
+      }
+    }
+    scene
+  }
 }
